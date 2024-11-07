31d11ce3f9546d8b4c436320690283747c6cbb6d
nathankutella <nathanKutella752@gmail.com>
2024-11-07 14:00:19 -0600

    FULL COMPLETE

--- my-sportswebsite/src/components/NavBar.js
+++ my-sportswebsite/src/components/NavBar.js
@@ -104,7 +104,7 @@ const NavBar = () => {
                 {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
             <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed hidden'}>
-                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>MY PORTFOLIO.</h1>
+                <h1 className='w-full text-2xl font-bold text-[#00df9a] m-4'>NFL Sports Tracker</h1>
                 <ul className='uppercase p-4'>
                     <li className='p-4 border-b border-gray-600'>
                         <Link 
